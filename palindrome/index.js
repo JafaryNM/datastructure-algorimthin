63abdc9b7072c0464f1502faf7853bcd20fe0c8f
Jafary Mdegela <jaffarymdegela@gmail.com>
2025-12-24 08:47:34 +0300

    integer reversal

--- palindrome/index.js
+++ palindrome/index.js
@@ -8,8 +8,12 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  const reverse = str.split("").reverse().join("");
-  return str === reverse;
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
+
+//   const reverse = str.split("").reverse().join("");
+//   return str === reverse;
