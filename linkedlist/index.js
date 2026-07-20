6c2c54722534b5cd2a2930efa33d88501b1260dd
JafaryNm <Jaffarymdegela@gmail.com>
2026-07-20 12:49:11 +0300

    get size of node

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -17,6 +17,14 @@ class LinkedList {
     const node = new Node(data, this.head);
     this.head = node;
   }
+  size() {
+    let counter = 0;
+    let node = this.node;
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+  }
 }
 const list = new LinkedList();
 list.Node = new Node(10);
